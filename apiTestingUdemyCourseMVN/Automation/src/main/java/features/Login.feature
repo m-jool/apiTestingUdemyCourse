5f0f5bb6a35ba6ead343517f6a9025018e8cd12c
Joseph Loutfi <joseph@anghami.com>
2022-09-29 11:49:27 +0400

    chore: add info and cases on passing arguments to fuctions in Gherkin

--- apiTestingUdemyCourseMVN/Automation/src/main/java/features/Login.feature
+++ apiTestingUdemyCourseMVN/Automation/src/main/java/features/Login.feature
@@ -2,6 +2,12 @@ Feature: Application Login
 
   Scenario: Home page default login
     Given User is on NetBanking landing page
-    When User login into application with user and password
-    Then Home page is populated
+    When User login into application with user "jin" and password "1234"
+    Then Home page 1 is populated
+    And Card are displayed
+
+  Scenario: Home page default login
+    Given User is on NetBanking landing page
+    When User login into application with user "john" and password "4321"
+    Then Home page 2 is populated
     And Card are displayed
--- apiTestingUdemyCourseMVN/Automation/src/main/java/step_definitions/StepDefinition.java
+++ apiTestingUdemyCourseMVN/Automation/src/main/java/step_definitions/StepDefinition.java
@@ -12,16 +12,26 @@ public class StepDefinition {
         System.out.println("hello testing! 1");
     }
 
-    @When("User login into application with user and password")
-    public void userLoginIntoApplicationWithUserAndPassword() {
-        //write code here
-        System.out.println("hello testing! 1");
+//    @When("User login into application with user and password")
+//    public void userLoginIntoApplicationWithUserAndPassword() {
+//        //write code here
+//        System.out.println("hello testing! 1");
+//    }
+
+    @When("User login into application with user {string} and password {string}")
+    public void userLoginIntoApplicationWithUserAndPassword(String arg0, String arg1) {
+        System.out.println("hello testing! " + arg0 + arg1);
     }
 
-    @Then("Home page is populated")
-    public void homePageIsPopulated() {
-        //write code here
-        System.out.println("hello testing! 1");
+//    @Then("Home page is populated")
+//    public void homePageIsPopulated() {
+//        //write code here
+//        System.out.println("hello testing! 1");
+//    }
+
+    @Then("Home page {int} is populated")
+    public void homePageIsPopulated(int arg0) {
+        System.out.println("hello testing! " + arg0);
     }
 
     @And("Card are displayed")
